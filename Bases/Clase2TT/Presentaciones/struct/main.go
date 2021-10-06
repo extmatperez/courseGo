20bb58208ce15eed0c16a09ff2987adef9dc0e1a
Matias Perez <mataguper20@gmail.com>
2021-10-06 11:20:23 -0300

    struct dentro de struct

--- Bases/Clase2TT/Presentaciones/struct/main.go
+++ Bases/Clase2TT/Presentaciones/struct/main.go
@@ -1,1 +1,43 @@
 package main
+
+import "fmt"
+
+type Fecha struct {
+	dia  int
+	mes  int
+	anio int
+}
+
+type Persona struct {
+	nombre          string
+	edad            int
+	fechaNacimiento Fecha
+}
+
+func main() {
+	p1 := Persona{"Matias", 26, Fecha{17, 11, 1994}}
+
+	p2 := Persona{
+		nombre:          "Matias2",
+		edad:            26,
+		fechaNacimiento: Fecha{17, 11, 1994},
+	}
+
+	var p3 Persona
+	p3.nombre = "Matias"
+
+	var p4 Persona
+	p4.edad = 26
+
+	// var p5 Persona{}
+	// p5.nombre = "Matias"
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Println(p3)
+	fmt.Println(p4)
+
+	p4 = p1
+	p4.fechaNacimiento.dia = 16
+	fmt.Println(p4)
+}
