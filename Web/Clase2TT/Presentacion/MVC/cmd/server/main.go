799385565dcfa22371194e9bf5630027d8099f93
Matias Perez <mataguper20@gmail.com>
2021-10-13 11:06:21 -0300

    Agregado de dependencias

---
+++ Web/Clase2TT/Presentacion/MVC/cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/extmatperez/courseGo/Web/Clase2TT/Presentacion/MVC/cmd/server/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	repo := products.NewRepository()
+	service := products.NewService(repo)
+	p := handler.NewProduct(service)
+
+	r := gin.Default()
+	pr := r.Group("/products")
+	pr.POST("/", p.Store())
+	pr.GET("/", p.GetAll())
+	r.Run()
+}
