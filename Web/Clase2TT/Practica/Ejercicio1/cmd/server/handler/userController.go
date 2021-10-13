4471af74963755ced2ff4fd79019dd4b173211d8
Matias Perez <mataguper20@gmail.com>
2021-10-13 13:42:33 -0300

    Modificacion

--- Web/Clase2TT/Practica/Ejercicio1/cmd/server/handler/userController.go
+++ Web/Clase2TT/Practica/Ejercicio1/cmd/server/handler/userController.go
@@ -1,1 +1,69 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	Nombre          string  `json:"nombre"`
+	Apellido        string  `json:"apellido"`
+	Email           string  `json:"email"`
+	Edad            int     `json:"edad"`
+	Altura          float64 `json:"altura"`
+	Activo          bool    `json:"activo"`
+	FechaDeCreacion string  `json:"fechaDeCreacion"`
+}
+
+type User struct {
+	service usuarios.userService
+}
+
+func NewUser(p usuarios.userService) *User {
+	return &Usuario{
+		service: p,
+	}
+}
+
+func (c *Product) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "123456" {
+			ctx.JSON(401, gin.H{
+				"error": "token inválido",
+			})
+			return
+		}
+
+		p, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(404, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(200, p)
+	}
+}
+
+func (c *Product) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "123456" {
+			ctx.JSON(401, gin.H{"error": "token inválido"})
+			return
+		}
+		var req request
+		if err := ctx.Bind(&req); err != nil {
+			ctx.JSON(404, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		p, err := c.service.Store(req.Name, req.Type, req.Count, req.Price)
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(200, p)
+	}
+}
--- Web/Clase2TT/Practica/Ejercicio1/cmd/server/main.go
+++ Web/Clase2TT/Practica/Ejercicio1/cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	usuarios "github.com/extmatperez/courseGo/Web/Clase2TT/Practica/Ejercicio1/internal/usuarios"
+)
+
 func main() {
+	repo := usuarios.NewRepository()
+	serv := usuarios.NewService(repo)
 
 }
