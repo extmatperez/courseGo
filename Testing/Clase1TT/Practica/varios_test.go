b471dc2b03477d55830b364bd63a6552e8d4cfd8
Matias Perez <mataguper20@gmail.com>
2021-10-26 19:38:59 -0300

    Coverage

--- Testing/Clase1TT/Practica/varios_test.go
+++ Testing/Clase1TT/Practica/varios_test.go
@@ -1,6 +1,8 @@
 package ordenamientos
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,11 +18,50 @@ func TestOrdenar(t *testing.T) {
 
 }
 
-func TestDividir(t *testing.T) {
+func TestDividirError(t *testing.T) {
 	num1, num2 := 5, 0
 
 	_, err := Dividir(num1, num2)
 
 	assert.NotNil(t, err)
 
+}
+
+func TestDividir(t *testing.T) {
+	num1, num2 := 6, 2
+
+	num, _ := Dividir(num1, num2)
+
+	assert.Equal(t, 3, num)
+
+}
+
+func TestSumar(t *testing.T) {
+	num1, num2 := 5, 0
+
+	num := Sumar(num1, num2)
+
+	assert.Equal(t, 5, num)
+
+}
+func TestRestar(t *testing.T) {
+	num1, num2 := 5, 0
+
+	num := Restar(num1, num2)
+
+	assert.Equal(t, 5, num)
+
+}
+
+func BenchmarkSum256(b *testing.B) {
+	data := []byte("Digital House impulsando la transformacion digital")
+	for i := 0; i < b.N; i++ {
+		sha256.Sum256(data)
+	}
+}
+func BenchmarkSum(b *testing.B) {
+	data := []byte("Digital House impulsando la transformacion digital")
+	for i := 0; i < b.N; i++ {
+		sha1.Sum(data)
+	}
 }
